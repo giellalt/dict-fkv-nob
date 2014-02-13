8d81fd789ab1da21e7e1f55d28af98456d74a0a5
Anna-Kaisa Räisänen <anna-kaisa.raisanen@kvenskinstitutt.no>
2014-02-13 11:18:54 +0000

    This one does not work either. Debug: Look at bin/fkvnob.lexc

--- fkvnob.sh
+++ fkvnob.sh
@@ -3,20 +3,38 @@
 # ei fst-fil som tar fkv og gjev ei nob-omsetjing.
 
 # Førebels er det berre eit shellscript.
-
 # Kommando for å lage fkvnob.fst
+
+# sh fkvnob.sh
+
+echo 
+echo "Etter at dette scriptet er ferdig står du i xfst med promten"
+echo "xfst[1]"
+echo 
+echo "Gjör då dette:"
+echo "invert"
+echo "save bin/fkvnob.fst"
+echo "quit"
+echo ""
+echo "LEXICON Root" > bin/nobfkv.lexc
+
+cat  src/*_nobfkv.xml | \
+
 echo "LEXICON Root" > bin/fkvnob.lexc
-cat src/*_fkvnob.xml | grep '<[lt] '|tr '\n' '™' | sed 's/<l /£/g;'| tr '£' '\n'| grep -v '\!'|sed 's/<re>[^>]*>//g;'|tr '<' '>'| cut -d">" -f2,6| tr ' ' '_'| tr '>' ':'| grep -v '__'|sed 's/$/ # ;/g' >> bin/fkvnob.lexc
-#xfst -e "read lexc < bin/fkvnob.lexc"
 
-printf "read lexc < bin/fkvnob.lexc \n\
-save stack bin/nobfkv.fst \n\
-invert net \n\
-save stack bin/fkvnob.fst \n\
-quit \n" > tmpfile
-xfst -utf8 < tmpfile
-rm -f tmpfile
+cat src/*_fkvnob.xml | \
+grep '^ *<' | \
+grep -v xml | \
+grep -v '_' | \
+tr '\n' '™' | sed 's/<e/£/g;'| tr '£' '\n'| \
+sed 's/<re>[^>]*>//g;'|tr '<' '>'| cut -d">" -f6,16| \
+tr ' ' '_'| sed 's/:/%/g;'|tr '>' ':'| \
+grep -v '__'|sed 's/$/ # ;/g' >> bin/fkvnob.lexc
 
-# deretter i xfst:
+xfst -e "read lexc < bin/fkvnob.lexc"
+
+
+# deretter gjer du dette i xfst:
 # invert
 # save bin/fkvnob.fst
+
